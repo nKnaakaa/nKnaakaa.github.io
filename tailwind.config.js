4234858c4c88b38d09179e2d95c3454c2ac536b5
naka <joaovtnakamura@gmail.com>
2023-01-14 21:21:22 -0300

    the penis monkey

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,17 @@ module.exports = {
         extend: {
             keyframes: {
                 type: {
-                    '0%': { width: '0ch' },
-                    '5%, 10%': { width: '1ch' },
-                    '15%, 20%': { width: '2ch' },
-                    '25%, 30%': { width: '3ch' },
-                    '35%, 40%': { width: '4ch' },
-                    '45%, 50%': { width: '5ch' },
-                    '55%, 60%': { width: '6ch' },
-                    '65%, 70%': { width: '7ch' },
-                    '75%, 80%': { width: '8ch' },
-                    '85%, 90%': { width: '9ch' },
-                    '95%': { width: '10ch' },
+                    "0%": { width: '0ch' },
+                    "5%, 10%": { width: '1ch' },
+                    "15%, 20%": { width: '2ch' },
+                    "25%, 30%": { width: '3ch' },
+                    "35%, 40%": { width: '4ch' },
+                    "45%, 50%": { width: '5ch' },
+                    "55%, 60%": { width: '6ch' },
+                    "65%, 70%": { width: '7ch' },
+                    "75%, 80%": { width: '8ch' },
+                    "85%, 90%": { width: '9ch' },
+                    "95%": { width: '10ch' },
                 },
                 blink: {
                     "50%": { opacity: 0 }
@@ -23,7 +23,7 @@ module.exports = {
 
             animation: {
                 type: 'type 1.25s ease-out .5s 1 normal both',
-                blink: "blink 1s step-start infinite",
+                blink: "blink 1s step-start infinite"
             }
         }
     }
